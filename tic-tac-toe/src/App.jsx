05b8e4a9667420f4eac30d979a6980d0d1048258
salimraza786 <razasalimraza17@gmail.com>
2023-10-15 21:36:03 +0530

    first commit

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './Components/Card/Card'
+import Grid from './Components/Grid/Grid'
+function App() {
+ 
+
+  return (
+    <>
+     <Grid numberOfCards = {9}/>
+    </>
+  )
+}
+
+export default App
